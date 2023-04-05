3dfb64d2ec12ef25a283bb87922dfe43870348f3
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2023-04-04 18:55:55 -0600

    edit price

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -3,12 +3,38 @@ import React, { useState } from "react";
 function PlantCard({ plant }) {
 
   const [inStock, setInStock] = useState(true)
+  const [showEdit, setShowEdit] = useState(false)
+  const [plantPrice, setPlantPrice] = useState(plant.price)
+
+  const handleChangePrice = (e) => {
+    fetch(`http://localhost:6001/plants/${plant.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({...plant, price: plantPrice})
+    })
+    .then(resp => resp.json())
+    .then(plant => {
+      setShowEdit(false)
+    })
+  }
 
   return (
     <li className="card">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
-      <p>Price: {plant.price}</p>
+      {showEdit ? 
+        <div>
+          <input value={plantPrice} onChange={(e) => setPlantPrice(e.target.value)} type="text"></input>
+          <button onClick={handleChangePrice}>Change Price</button>
+        </div>
+        :
+        <div>
+          <p>Price: {plantPrice}</p>
+          <button onClick={() => setShowEdit(true)}>✏️</button>
+        </div>
+      }
       {inStock ? (
         <button onClick={() => setInStock(false)} className="primary">In Stock</button>
       ) : (
